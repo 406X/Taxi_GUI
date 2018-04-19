d33944adf22261302634b970126d97caf10259b0
406X <406X@DESKTOP-FFF7B4T>
2018-04-20 01:35:40 +0800

    Some basic features implemented

--- src/taxi_gui/Main.java
+++ src/taxi_gui/Main.java
@@ -1,8 +1,14 @@
 package taxi_gui;
 
+
 import java.util.Arrays;
+import static javafx.animation.Animation.INDEFINITE;
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -16,6 +22,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 public class Main extends Application {
 	
 	
@@ -34,9 +41,10 @@ public class Main extends Application {
 	private int maxPassenger = 10;
 	private int[][][] list = new int[maxPassenger][3][2];
 	private int[][] Taxi = new int[maxTaxi][2];
-	private int[][] blockWeight = new int[boxes][boxes];
+	private int[][] blockWeight = new int[boxes+1][boxes+1];
+	private long time =0;
+	Taxi Taxii = new Taxi(maxPassenger,1,boxes);
 	
-			
 	Color[] colorPassenger = new Color[10];
 	mainCanvas canvas = new mainCanvas(canvasWidth,canvasHeight);
 	GraphicsContext gc_box = canvas.getGraphicsContext2D();
@@ -44,10 +52,11 @@ public class Main extends Application {
 	int box_lineWidth = 4;
 	GraphicsContext gc_taxi = canvas.getGraphicsContext2D();
 	GraphicsContext gc_weight = canvas.getGraphicsContext2D();
+	GraphicsContext gc = canvas.getGraphicsContext2D();
 	@Override
 	public void start(Stage primaryStage) {
 		//GUI
-		primaryStage.setTitle("Taxii Simulator!");
+		primaryStage.setTitle("Taxii Simulator! Time Elapsed: " + time + " seconds");
 		
 		Group root = new Group();
 		Scene scene = new Scene(root);
@@ -105,35 +114,61 @@ public class Main extends Application {
 		addPassenger.setTranslateY(y_srcx+20);
 		root.getChildren().add(addPassenger);
 		
+		
+		
+		
+		
+	
+		
+		
 		//Init Variables
 		for(int count = 0 ; count< boxes ;count++)
 			Arrays.fill(blockWeight[count], 1);
 		
 		//Test Variables
-		numPassenger=2;
-		list[0][0][0]=1;
-		list[0][0][1]=1;
-		list[0][1][0]=5;
-		list[0][1][1]=5;
-		list[0][2][0]=1;
-		list[0][2][1]=1;
 		
-		list[1][0][0]=2;
-		list[1][0][1]=2;
-		list[1][1][0]=5;
-		list[1][1][1]=5;
-		list[1][2][0]=2;
-		list[1][2][1]=2;
+
+		addPassenger(2,2,4,4);
+		addPassenger(8,8,3,3);
+		addTaxi();
 		
-		numTaxi=1;
-		Taxi[0][0] = 2;
-		Taxi[0][1] = 3;
+		primaryStage.show();
 		
+		list = Taxii.getPassengerCoords();
+		Taxi = Taxii.getTaxiCoords();
 		drawBoxes();
 		drawBlocks();
-		drawWeight();
 		drawTaxi();
-		primaryStage.show();
+		drawWeight();
+
+		Timeline timeline = new Timeline(
+			 new KeyFrame(Duration.seconds(1), e -> {
+			 gc.setFill( Color.WHITE);
+				gc.fillRect(0, 0, canvasWidth, canvasHeight);
+				Taxii.move();
+				numPassenger = Taxii.getNumPassenger();
+				list = Taxii.getPassengerCoords();
+				Taxi = Taxii.getTaxiCoords();
+				drawBoxes();
+				drawBlocks();
+				drawTaxi();
+				drawWeight();
+				time+=1;
+				primaryStage.setTitle("Taxii Simulator! Time Elapsed: " + time +" seconds");
+			})
+		);
+		timeline.setCycleCount(INDEFINITE);
+		
+		timeline.play();
+		
+		addPassenger.setOnAction( 
+				new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent e) {
+				addPassenger(Integer.valueOf(tf_srcx.getText()),Integer.valueOf(tf_srcy.getText()) ,Integer.valueOf(tf_dstx.getText()),Integer.valueOf(tf_dsty.getText()));
+			}
+		});
+		
 	}//End of start
 	
 	
@@ -141,7 +176,18 @@ public class Main extends Application {
 		launch(args);
 	}
 	
+	public void addPassenger(int x_src,int y_src,int x_dest,int y_dest){
+		Taxii.add(y_src, x_src, x_dest, y_dest);
+		numPassenger++;
+	}
+	
+	public void addTaxi(){
+		Taxii.addTaxi();
+		numTaxi++;
+	}
+	
 	public void drawBoxes(){
+		gc_box.setStroke(Color.BLACK);
 		for(int count = 0 , x = 0 , y = 0 ; count<boxes ; count++, y+=sqrHeight, x = 0){
 			for(int count2 = 0 ; count2 < boxes ; count2++, x+=sqrWidth){
 				gc_box.strokeRect(x, y, sqrWidth, sqrHeight);
@@ -151,11 +197,9 @@ public class Main extends Application {
 		gc_box.strokeRect(0, 0, canvasWidth, canvasHeight);
 	}
 	
-	public void move(int index, int c_x , int c_y){
-		
-	}
 	
 	public void drawBlocks(){
+		gc_blocks.setFill(Color.BLACK);
 		int[][] countBlock = new int[boxes][boxes];
 		for(int count = 0 ;count < numPassenger ; count++){
 			gc_blocks.fillRect( convertX(list[count][2][0],list[count][2][1],countBlock) , convertY(list[count][2][0],list[count][2][1],countBlock), sqrWidth/2, sqrHeight/2);
@@ -167,11 +211,12 @@ public class Main extends Application {
 		gc_taxi.setLineWidth(box_lineWidth/2);
 		gc_taxi.setStroke( Color.AQUA);
 		for(int count = 0 ; count < numTaxi ; count++){
-			gc_taxi.strokeRect(Taxi[count][0]*sqrWidth + box_lineWidth/2 , Taxi[count][0]*sqrHeight + box_lineWidth/2, sqrWidth - box_lineWidth, sqrHeight - box_lineWidth);
+			gc_taxi.strokeRect((Taxi[count][0] - 1)*sqrWidth + box_lineWidth/2 , (Taxi[count][1] - 1)*sqrHeight + box_lineWidth/2, sqrWidth - box_lineWidth, sqrHeight - box_lineWidth);
 		}
 	}
 	
 	public void drawWeight(){
+		gc_weight.setStroke(Color.BLACK);
 		gc_weight.setFont(new Font("Verdana", 14));
 		gc_weight.setLineWidth(1);
 		for(int count = 0 , x = 0 , y = 0 ; count<boxes ; count++, y+=sqrHeight, x = 0){
--- src/taxi_gui/Taxi.java
+++ src/taxi_gui/Taxi.java
@@ -6,11 +6,11 @@ package taxi_gui;
 import java.util.Arrays;
 
 public class Taxi {
-  
-    private int numPassenger = 0;
-    private int[][][] passenger;
+	
+	private int numPassenger = 0;
+	private int[][][] passenger;
 	private int numTaxi = 0;
-	private int maxTaxi = 4;
+	private int maxTaxi = 1;
 	private int maxPassenger = 10;
 	private int taxiSize = 2;
 	private int boxes =8;
@@ -20,7 +20,7 @@ public class Taxi {
 	private int[][] taxiCoords = new int[maxTaxi][2];
 	private int[][] taxiPassenger = new int[maxTaxi][taxiSize];
 	private int[] taxiNumPassenger = new int[maxTaxi];
-	private int[][] blockWeight = new int[boxes][boxes];
+	private int[][] blockWeight = new int[boxes+1][boxes+1];
 	private int[] passengerStatus = new int[maxPassenger];
 	private int[] taxiWeight = new int[maxTaxi];
 	private int[] taxiFetching = new int[maxTaxi];
@@ -35,88 +35,109 @@ public class Taxi {
 	*/
 	
 	
-    /*
-    passengerCoords[ID][0] = Source
-    passengerCoords[ID][1] = Destination
-    passengerCoords[ID][2] = Current
-    */
-    
-    public Taxi(int maxPassenger, int taxiSize, int boxes){
-        this.maxPassenger = maxPassenger;
+	/*
+	passengerCoords[ID][0] = Source
+	passengerCoords[ID][1] = Destination
+	passengerCoords[ID][2] = Current
+	*/
+	
+	public Taxi(int maxPassenger, int taxiSize, int boxes){
+		this.maxPassenger = maxPassenger;
 		this.taxiSize = taxiSize;
 		this.boxes = boxes;
+		
+		
+		Arrays.fill(taxiFetching, -1);
+		taxiPassenger = new int[maxTaxi][taxiSize];
+		
 		for(int count = 0 ; count<maxTaxi ; count++)
 			Arrays.fill( taxiPassenger[count], -1);
 		
-		Arrays.fill(taxiFetching, -1);
-    }
-    
-    
-    public void add(int s_x ,int s_y ,int d_x ,int d_y){
-        if(numPassenger<maxPassenger){
-            numPassenger++;
-            int[][][] templist = new int[numPassenger][3][2];
-            templist = passenger.clone();
-            passenger[numPassenger-1][0][0] = s_x;
-            passenger[numPassenger-1][0][1] = s_y;
-            passenger[numPassenger-1][1][0] = d_x;
-            passenger[numPassenger-1][1][1] = d_y;
-            passenger = templist.clone();
-        }
-    }
-    
-    public void remove(int index){
-        
-        int[][][] templist = new int[numPassenger-1][4][2];
-        
-        for(int count = 0 ,count2 = 0; count < numPassenger ; count++){
-            if(count==index-1)
-                continue;
-            templist[count2] = passenger[count].clone();
-            count2++;
-        }
-        passenger = templist.clone();
-        numPassenger--;
-    }
-    
-    public int[][][] getPassengerCoords(){
-        return passenger;
-    }
-    
-    public int[][] getTaxiCoords(){
-        return taxiCoords;
-    }
+		passenger = new int[this.maxPassenger][3][2];
+	}
+	
+	
+	public void add(int s_x ,int s_y ,int d_x ,int d_y){
+		if(numPassenger<maxPassenger){
+			numPassenger++;
+			System.out.println(numPassenger);
+			passenger[numPassenger-1][0][0] = s_x;
+			passenger[numPassenger-1][0][1] = s_y;
+			passenger[numPassenger-1][1][0] = d_x;
+			passenger[numPassenger-1][1][1] = d_y;
+			passenger[numPassenger-1][2][0] = s_x;
+			passenger[numPassenger-1][2][1] = s_y;
+		}
+	}
+	
+	public void addTaxi(){
+		numTaxi++;
+		taxiCoords[numTaxi-1][0] = 1;
+		taxiCoords[numTaxi-1][1] = 1;
+	}
+	
+	public void dropOff(int taxiIndex , int passengerIndex){
+		remove(passengerIndex);
+		taxiPassenger[taxiIndex][0]=-1;	
+		taxiNumPassenger[taxiIndex]--;
+	}
+	
+	public void remove(int index){
+		
+		int[][][] templist = new int[maxPassenger][3][2];
+		
+		for(int count = 0 ,count2 = 0; count < numPassenger ; count++){
+			if(count==index)
+				continue;
+			
+			templist[count2] = passenger[count].clone();
+			count2++;
+		}
+		passenger = templist.clone();
+		numPassenger--;
+	}
+	
+	public int[][][] getPassengerCoords(){
+		return passenger;
+	}
+	
+	public int[][] getTaxiCoords(){
+		return taxiCoords;
+	}
 	
 	public int[] getTaxiCoords(int index){
-         return taxiCoords[index];
-    }
-    
-    public int getClosestPassenger(int index){
-        int[] distance = new int[numPassenger];
-        int closestIndex = 0;
-        
-        for(int count = 0 ; count < numPassenger ; count++){
-            distance[count] =  ( taxiCoords[index][0] - passenger[count][0][0] ) + taxiCoords[index][1] - passenger[count][0][1];
-            
-            if(distance[count]<0)
-                distance[count]*=-1;
-            
-            if( distance[count] < distance[closestIndex] && passengerStatus[count]==0)
-                closestIndex = count;
-                
-        }
-        return closestIndex;
-    }
-    
-    public void move(){
-        
-        
+		return taxiCoords[index];
+	}
+	
+	public int getClosestPassenger(int index){
+		int[] distance = new int[numPassenger];
+		int closestIndex = 0;
+		
+		for(int count = 0 ; count < numPassenger ; count++){
+			distance[count] =  ( taxiCoords[index][0] - passenger[count][0][0] ) + taxiCoords[index][1] - passenger[count][0][1];
+			
+			if(distance[count]<0)
+				distance[count]*=-1;
+			
+			if( distance[count] < distance[closestIndex] && passengerStatus[count]==0)
+				closestIndex = count;
+			
+		}
+		return closestIndex;
+	}
+	
+	public void move(){
+		
+		
 		for(int count = 0 ; count < numTaxi ; count++){
+			if(numPassenger==0)
+						continue;
+			
 			//If no passenger
 			if( taxiIsEmpty(count)){
-				
-				
 				int fetchID = getClosestPassenger(count);
+				
+				//If taxi is no fetching anyone
 				if(taxiFetching[count]==-1){
 					passengerStatus[fetchID]=1;
 					taxiFetching[count] = fetchID;
@@ -126,38 +147,90 @@ public class Taxi {
 					taxiFetching[count] = fetchID;
 				}
 				
-				if(taxiCoords[count]==passenger[fetchID][2]){
+				//If taxi is at passenger
+				if(taxiCoords[count][0]==passenger[fetchID][2][0] && taxiCoords[count][1]==passenger[fetchID][2][1] ){
 					taxiPassenger[count][taxiNumPassenger[count]] = fetchID;
 					taxiNumPassenger[count]++;
 					taxiFetching[count]=-1;
+					
+					int[] movement = moveTaxi( taxiCoords[count],passenger[fetchID][1],count);
+					taxiCoords[count][0]+= movement[0];
+					taxiCoords[count][1]+= movement[1];
+					passenger[fetchID][2][0] += movement[0];
+					passenger[fetchID][2][1] += movement[1];
+					continue;
 				}
 				else{
-					int[] movement = moveAlgo( taxiCoords[count],passenger[fetchID][2]);
+					//Taxi Movement
+					int[] movement = moveTaxi( taxiCoords[count],passenger[fetchID][2],count);
 					taxiCoords[count][0]+= movement[0];
 					taxiCoords[count][1]+= movement[1];
 				}
-				
 			}
 			//If have passenger
 			else{
-				
 				//If at destination
-				
+				if(taxiCoords[count][0]==passenger[  taxiPassenger[count][0] ][1][0] && taxiCoords[count][1]==passenger[ taxiPassenger[count][0] ][1][1] ){
+					dropOff( count , taxiPassenger[count][0]);
+					
+					if(numPassenger==0)
+						continue;
+					
+					count--;
+					continue;
+				}
 				//If not at destination
 				
+				int[] movement = moveTaxi( taxiCoords[count],passenger[ taxiPassenger[count][0] ][1],count);
+				taxiCoords[count][0]+= movement[0];
+				taxiCoords[count][1]+= movement[1];
+				passenger[  taxiPassenger[count][0] ][2][0] += movement[0];
+				passenger[  taxiPassenger[count][0] ][2][1] += movement[1];
 				
 			}
 		}
-    }
+	}
 	
-	public int[] moveAlgo( int[] src , int[] dest){
+	public int getNumPassenger(){
+		return numPassenger;
+	}
+	
+	
+	public int[] moveTaxi( int[] src , int[] dest, int taxiIndex){
 		int[] movement = new int[2];
-	
-		//Movement Algo goes here
 		
+		//Movement Algo goes here
+		if(blockWeight[ src[1] ][ src[0]] >1 && taxiWeight[taxiIndex] < blockWeight[ src[1] ][ src[0]])
+			return movement;
+		
+		int x_diff = dest[0] - src[0];
+		int y_diff = dest[1] - src[1];
+		
+		int x_diff_abs = x_diff;
+		int y_diff_abs = y_diff;
+		
+		if(x_diff_abs<0)
+			x_diff_abs*=-1;
+		
+		if(y_diff_abs<0)
+			y_diff_abs*=-1;
+		
+		if(x_diff_abs>=y_diff_abs){
+			if( x_diff > 0)
+				movement[0] = 1;
+			else 
+				movement[0] = -1;
+		
+		}
+		else{
+			if( y_diff > 0)
+				movement[1] = 1;
+			else 
+				movement[1] = -1;
+		}
 		return movement;
 	}
-    
+	
 	public boolean taxiIsEmpty(int index){
 		for(int count = 0 ; count < taxiSize ; count++)
 			if(taxiPassenger[index][count]>=0)
@@ -166,9 +239,9 @@ public class Taxi {
 	}
 	
 	//For Passenger
-    public void updateCoords(int index, int c_x , int c_y){
-        passenger[index-1][3][0] = c_x;
-        passenger[index-1][3][1] = c_y;
-    }
-    
+	public void updateCoords(int index, int c_x , int c_y){
+		passenger[index-1][3][0] = c_x;
+		passenger[index-1][3][1] = c_y;
+	}
+	
 }
